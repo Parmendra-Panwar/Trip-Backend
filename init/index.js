67664b0c8973a45f0b84dace5099d6e2dddc6b23
Parmendra-Panwar <panwparmendra7@gmail.com>
2026-02-17 18:49:32 +0530

    Cradential FIx3

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.EXPO_PUBLIC_API_URL;
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
   .then(() => {
